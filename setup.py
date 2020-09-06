30545a18b7a6cc26ec57d49720b1a6e8604f90e8
yu9824 <58211916+yu9824@users.noreply.github.com>
2020-09-06 22:13:36 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as requirements_file:
+    install_requirements = requirements_file.read().splitlines()
+
+setup(
+    name='ternary-diagram',
+    version='0.0.1',
+    description='You can make ternary diagram easily.',
+    author='yu-9824',
+    author_email='yu.9824@gmail.com',
+    install_requires=install_requirements,
+    url='https://github.com/yu-9824/ternary-diagram',
+    license=license,
+    packages=find_packages(exclude=['example'])
+)
