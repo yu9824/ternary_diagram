7e66a4a68940431bdbac6f2602613b05befd7b2a
Yudai Iwamizu <yu.9824@gmail.com>
2020-09-06 22:35:09 +0900

    Edit setup.py (rename ternary-diagram to ternary_diagram)

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ with open('requirements.txt') as requirements_file:
     install_requirements = requirements_file.read().splitlines()
 
 setup(
-    name='ternary-diagram',
+    name='ternary_diagram',
     version='0.0.1',
     description='You can make ternary diagram easily.',
     author='yu-9824',
     author_email='yu.9824@gmail.com',
     install_requires=install_requirements,
-    url='https://github.com/yu-9824/ternary-diagram',
+    url='https://github.com/yu-9824/ternary_diagram',
     license=license,
     packages=find_packages(exclude=['example'])
 )
