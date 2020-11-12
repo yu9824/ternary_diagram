e34b8c0b1a1daeaa1664a338ef99eafebe79505a
Yudai Iwamizu <yu.9824@gmail.com>
2020-11-12 12:38:13 +0900

    Upgrade version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as requirements_file:
 
 setup(
     name='ternary_diagram',
-    version='0.0.5',
+    version='0.0.6',
     description='You can make ternary diagram easily.',
     author='yu-9824',
     author_email='yu.9824@gmail.com',
