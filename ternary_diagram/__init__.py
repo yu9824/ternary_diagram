4fe8e76f278177d2684e855ecae42bda469c0260
yu9824 <58211916+yu9824@users.noreply.github.com>
2021-04-24 13:00:30 +0900

    Change many things to register PyPI. * __init__.py (Add __version__ or something.) * Add MANIFEST.in (Define which file have to be included or not.) * setup.py (read __init__.py to get version, author, and so on.)

--- ternary_diagram/__init__.py
+++ ternary_diagram/__init__.py
@@ -1,1 +1,14 @@
 from .ternary_diagram import *
+
+__version__      = '1.0.0'
+__license__      = 'MIT'
+__author__       = 'yu-9824'
+__copyright__    = 'Copyright (c) 2021 {0}'.format(__author__)
+__author_email__ = '{0}@{1}'.format('yu.9824.job', 'gmail.com')
+__user_id__ = 'yu-9824'
+__app_name__ = 'ternary_diagram'
+__url__ = 'http://github.com/{0}/{1}'.format(__user_id__, ___app_name__)
+
+__all__ = [
+    'main',
+    ]
