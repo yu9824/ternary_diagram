cb72654ac87a8feaa8a31f8a3f1742369beebb5e
Yudai IWAMIZU <yu.9824.job@gmail.com>
2021-05-12 09:56:50 +0900

    Modify test.py

--- ternary_diagram/test.py
+++ ternary_diagram/test.py
@@ -1,10 +1,16 @@
 from ternary_diagram import TernaryDiagram
 import matplotlib.pyplot as plt
 
+fig = plt.figure(facecolor = 'white', dpi = 100, figsize=(10, 4.8))
+ax1 = fig.add_subplot(121)
+ax2 = fig.add_subplot(122)
+
+ax1.plot([0, 1], [0, 1], c = 'black')
+
 materials = ['Li2O', 'La2O3', 'TiO2']
-td = TernaryDiagram(materials)
+td = TernaryDiagram(materials, fig = fig, ax = ax2)
 td.scatter([
     [1, 1, 1],
     ])
-
+plt.tight_layout()
 plt.show()
