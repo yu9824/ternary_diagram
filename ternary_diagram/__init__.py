451560c4bdcd71bb284c7ee5a08797754ebdea25
yu9824 <58211916+yu9824@users.noreply.github.com>
2023-07-22 16:45:38 +0900

    Formatting ternary_diagram/__init__.py

--- ternary_diagram/__init__.py
+++ ternary_diagram/__init__.py
@@ -1,14 +1,14 @@
 from .ternary_diagram import TernaryDiagram
 
-__version__      = '3.0.0dev0'
-__license__      = 'MIT'
-__author__       = 'yu9824'
-__copyright__ = 'Copyright © 2021 yu9824'
-__author_email__ = 'yu.9824.job@gmail.com'
-__user_id__ = 'yu9824'
-__app_name__ = 'ternary_diagram'
-__url__ = 'http://github.com/yu9824/ternary_diagram/'
+__version__ = "3.0.0dev0"
+__license__ = "MIT"
+__author__ = "yu9824"
+__copyright__ = "Copyright © 2021 yu9824"
+__author_email__ = "yu.9824.job@gmail.com"
+__user_id__ = "yu9824"
+__app_name__ = "ternary_diagram"
+__url__ = "http://github.com/yu9824/ternary_diagram/"
 
 __all__ = [
-    'TernaryDiagram',
+    "TernaryDiagram",
 ]
