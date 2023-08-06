804bd919cd7ab6b594667b9d895d0e778a421dda
yu9824 <58211916+yu9824@users.noreply.github.com>
2023-08-06 15:53:34 +0900

    Create some tests

--- tests/test_mono_scatter.py
+++ tests/test_mono_scatter.py
@@ -6,13 +6,12 @@ import pandas as pd
 @pytest.fixture
 def prepare_data() -> pd.DataFrame:
     df_mono_scatter = pd.read_csv(
-        "https://raw.githubusercontent.com/yu9824/ternary_diagram/main/example/mono_scatter/example_mono_scatter.csv"
+        "https://raw.githubusercontent.com/yu9824/ternary_diagram/main/example/mono_scatter/example_mono_scatter.csv"   # noqa
     )
     return df_mono_scatter
 
 
 def test_mono_scatter(prepare_data):
     df_mono_scatter: pd.DataFrame = prepare_data
-    print(df_mono_scatter)
     td = TernaryDiagram(materials=df_mono_scatter.columns)
     td.scatter(df_mono_scatter)
