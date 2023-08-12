9fafb333311979c3dbc2515559c9babb58edc65e
yu9824 <58211916+yu9824@users.noreply.github.com>
2023-08-12 17:21:39 +0900

    Rebuild document

--- docs_src/conf.py
+++ docs_src/conf.py
@@ -30,13 +30,6 @@ release = "v3.0.0"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-
-# to use markdown
-source_parsers = {
-    ".md": "recommonmark.parser.CommonMarkParser",
-}
-source_suffix = [".rst", ".md"]
-
 extensions = [
     "sphinx.ext.autodoc",  # docstringからドキュメントを作成してくれる．
     "sphinx.ext.napoleon",  # google式・numpy式docstringを整形してくれる．
@@ -45,13 +38,19 @@ extensions = [
     # "sphinx_markdown_tables",  # markdownの表を書けるようにする．
 ]
 
+# markdown
+source_suffix = [".rst", ".md"]
+source_parsers = {
+    ".md": "recommonmark.parser.CommonMarkParser",
+}
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "README.md"]
 
 
 # -- Options for HTML output -------------------------------------------------
--- docs_src/index.rst
+++ docs_src/index.rst
@@ -1,5 +1,5 @@
 .. ternary-diagram documentation master file, created by
-   sphinx-quickstart on Sat Aug 12 16:33:37 2023.
+   sphinx-quickstart on Sat Aug 12 17:09:54 2023.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
