c5b002996d14706c2ceb164f239c487267499c89
yu9824 <58211916+yu9824@users.noreply.github.com>
2023-08-12 16:49:59 +0900

    Update conf.py and format it.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,12 +17,12 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'ternary-diagram'
-copyright = '2023, yu9824'
-author = 'yu9824'
+project = "ternary-diagram"
+copyright = "2021, yu9824"
+author = "yu9824"
 
 # The full version, including alpha/beta/rc tags
-release = 'v3.0.0'
+release = "v3.0.0"
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,16 +30,27 @@ release = 'v3.0.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+source_parsers = {
+    ".md": "recommonmark.parser.CommonMarkParser",
+}
+
+source_suffix = [".rst", ".md"]
+
+extensions = [
+    "sphinx.ext.autodoc",  # docstringからドキュメントを作成してくれる．
+    "sphinx.ext.napoleon",  # google式・numpy式docstringを整形してくれる．
+    "sphinx.ext.githubpages",  # github-pages用のファイルを生成してくれる．
+    "recommonmark",  # markdownで書けるようにする．
+    # "sphinx_markdown_tables",  # markdownの表を書けるようにする．
+]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -47,9 +58,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
