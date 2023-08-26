db4b65b2e653130a97f27bd22676601e4e100c7e
yu9824 <58211916+yu9824@users.noreply.github.com>
2023-08-26 12:01:19 +0900

    v3.1.0dev0

--- ternary_diagram/__init__.py
+++ ternary_diagram/__init__.py
@@ -1,6 +1,6 @@
 from .ternary_diagram import TernaryDiagram
 
-__version__ = "3.0.1"
+__version__ = "3.1.0dev0"
 __license__ = "MIT"
 __author__ = "yu9824"
 __copyright__ = "Copyright © 2021 yu9824"
